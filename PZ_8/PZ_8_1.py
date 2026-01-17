47103f4c455115cae91663eb1a030c5b686bae06
HendiShmendi <alena_leonteva2017@mail.ru>
2026-01-17 12:30:05 +0300

    ьпл

---
+++ PZ_8/PZ_8_1.py
@@ -0,0 +1,25 @@
+# 1. Выведите 'history' из вложенного словаря:
+# sampleDict = {
+#     "class": {
+#         "student": {
+#             "name": "Mike",
+#             "marks": {
+#                 "physics": 70,
+#                 "history": 80
+#             }
+#         }
+#     }
+# }
+
+sampleDict = {
+    "class": {
+        "student": {
+            "name": "Mike",
+            "marks": {
+                "physics": 70,
+                "history": 80
+            }
+        }
+    }
+}
+print(sampleDict["class"]["student"]["marks"]["history"])
