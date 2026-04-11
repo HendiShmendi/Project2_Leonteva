719c86a2d08fc6c4f84fd1cfb1f817292a22a8a5
HendiShmendi <alena_leonteva2017@mail.ru>
2026-04-11 12:40:47 +0300

    ч

--- PZ_11/PZ_11_1.py
+++ PZ_11/PZ_11_1.py
@@ -1,9 +1,8 @@
 # 1. Дана последовательность целых чисел. Поменять местами ее первую и
 # последнюю трети.
 swap = lambda s: ' '.join(
-    (lambda a: a[len(a)*2//3:] + a[len(a)//3:len(a)*2//3] + a[:len(a)//3])(s.split())
+    (lambda a, n: a[-n:] + a[n:-n] + a[:n] if n > 0 else a)(s.split(), len(s.split()) // 3)
 )
 
-
-s="1 2 3 4 5 6 "
+s = "1 2 3 4 5 6 7 8 "
 print(swap(s))
